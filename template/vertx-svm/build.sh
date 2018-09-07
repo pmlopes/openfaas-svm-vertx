c8bb256b9c0241fb6788cfd87910fdc12c03839b
Paulo Lopes <paulo@mlopes.net>
2018-09-07 11:22:14 +0200

    clean up

--- template/vertx-svm/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-export PATH=~/.local/graalvm-ce-1.0.0-rc6/bin:$PATH
-export JAVA_HOME=~/.local/graalvm-ce-1.0.0-rc6
-
-./mvnw -Pnative-image -f entrypoint/pom.xml
-./mvnw -Pnative-image -f function/pom.xml
-./target/myapp-0.0.1-SNAPSHOT-fat
